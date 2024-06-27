61e19fb35167a33c5e4ada0ac9eeabe1e1e9dc55
junhung0818 <junhung0818@naver.com>
2024-06-27 17:09:08 +0900

    main화면 구성

---
+++ src/main/java/com/wintershop/shop/HelloController.java
@@ -0,0 +1,14 @@
+package com.wintershop.shop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/main")
+    public String home() {
+        return "HTML/index"; // HTML 디렉토리에 있는 index.html 파일을 가리킵니다.
+    }
+}
+
