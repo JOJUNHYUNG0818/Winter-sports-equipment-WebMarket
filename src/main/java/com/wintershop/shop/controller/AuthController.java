c064301b496250a05ef87173cc9f579d68d2d2ce
junhung0818 <junhung0818@naver.com>
2024-07-02 17:58:03 +0900

    로그인 구현

---
+++ src/main/java/com/wintershop/shop/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.wintershop.shop.controller;
+
+import com.wintershop.shop.model.User;
+import com.wintershop.shop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(User user) {
+        userService.save(user);
+        return "redirect:/login";
+    }
+}
